fa340514e9e8a2ff9451443ceca2d9a407ceec57
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-04-03 19:21:07 -0300

    WIP

--- hotdoc_c_extension/gi_extension.py
+++ hotdoc_c_extension/gi_extension.py
@@ -1024,7 +1024,7 @@ class GIExtension(Extension):
         out_parameters = []
 
         for i, param in enumerate (parameters):
-            if symbol.is_method and i == 0:
+            if isinstance(symbol, MethodSymbol) and i == 0:
                 continue
 
             direction = param.get_extension_attribute ('gi-extension', 'direction')
@@ -1318,13 +1318,14 @@ class GIExtension(Extension):
 
         gi_params, retval = self.__create_parameters_and_retval (node)
 
-        func = self.get_or_create_symbol(FunctionSymbol,
+
+        is_method = node.tag.endswith('method')
+        func = self.get_or_create_symbol(MethodSymbol if is_method else FunctionSymbol,
                                          parameters=gi_params,
                                          return_value=retval,
                                          display_name=name,
                                          unique_name=name,
                                          throws='throws' in node.attrib,
-                                         is_method=node.tag.endswith ('method'),
                                          is_constructor=node.tag==core_ns('constructor'),
                                          filename=self.__get_symbol_filename(name),
                                          parent_name=parent_name)
